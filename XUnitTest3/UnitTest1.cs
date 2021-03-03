d974e4a8b19677662902759c10bf413dc4f2d130
Chris Cooper <bloodwolf444@gmail.com>
2021-03-03 12:23:48 -0700

    Adding a unit test project

---
+++ XUnitTest3/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using Project1_tester_;
+
+
+
+
+namespace XUnitTest3
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           
+        }
+    }
+}
