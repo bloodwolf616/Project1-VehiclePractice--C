057329421161bf1f441942efe11908c7455413c5
Chris Cooper <bloodwolf444@gmail.com>
2021-07-26 15:36:32 -0700

    Created Garage class that encapsulates a list of 'AutoMobile' objects.

---
+++ Project1(tester)/Garage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_tester_
+{
+    class Garage
+    {
+
+        public List<AutoMobile> GarageCollection { get; set; }
+
+        /// <summary>
+        /// This class contains a collection of all of our Automobile objects.
+        /// </summary>
+        /// <param name="listOfAutomobiles">A list that contains objects of base type "AutoMobile"</param>
+        public Garage(List<AutoMobile> listOfAutomobiles)
+        {
+            GarageCollection = listOfAutomobiles;
+        }
+
+
+    }
+}
